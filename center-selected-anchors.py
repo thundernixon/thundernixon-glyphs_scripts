28947add0a55e52dd2787e8f27c57645fe842652
Stephen Nixon <swr410@gmail.com>
2018-11-27 09:54:50 -0500

    make round to whole number

--- center-selected-anchors.py
+++ center-selected-anchors.py
@@ -6,12 +6,13 @@ If an anchor is selected, it will be centered in the current glyph.
 Example:
 - If the glyph /H is `1624` units wide, the selected "top" 
   anchor will be moved to x = 1624/2 = 812.
+- Rounds up to nearest whole unit. If glyph width is `1189` units wide, selected anchors will move to `595`.
 """
 
 layer = Glyphs.font.selectedLayers[0] # current layer
 
 for anchor in layer.anchors:
     if anchor.selected == True:
-        centerXpos = layer.width/2
+        centerXpos = round(layer.width/2)
         anchor.x = centerXpos
-        print(anchor.name, " moved to ", str(centerXpos))
+        print(anchor.name + " moved to " + str(centerXpos))
